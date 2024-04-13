fbeb9a8209bbbeff641e31c8eb75a4b6c1b3955d
usama baloch <usamabaloch152001@gmail.com>
2024-04-13 21:43:44 +0500

    updated model.py file

--- Codes/model.py
+++ Codes/model.py
@@ -4,27 +4,11 @@ from torch import nn as nn
 
 import logging
 import logging.handlers
-
-root_logger = logging.getLogger()
-root_logger.setLevel(logging.INFO)
-
-# Some libraries attempt to add their own root logger handlers. This is
-# annoying and so we get rid of them.
-for handler in list(root_logger.handlers):
-    root_logger.removeHandler(handler)
-
-logfmt_str = "%(asctime)s %(levelname)-8s pid:%(process)d %(name)s:%(lineno)03d:%(funcName)s %(message)s"
-formatter = logging.Formatter(logfmt_str)
-
-streamHandler = logging.StreamHandler()
-streamHandler.setFormatter(formatter)
-streamHandler.setLevel(logging.DEBUG)
-
-root_logger.addHandler(streamHandler)
+from Codes.util_config import logging
 
 log = logging.getLogger(__name__)
 # log.setLevel(logging.WARN)
-# log.setLevel(logging.INFO)
+log.setLevel(logging.INFO)
 log.setLevel(logging.DEBUG)
 
 
